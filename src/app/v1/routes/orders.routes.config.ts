9f81854fe9dc9fc8608553644ff1da57dc4c05ed
hammadkhokhar <hello@hammadkhokhar.com>
2024-01-07 22:02:31 +0100

    setup routes for order controller

--- src/app/v1/routes/orders.routes.config.ts
+++ src/app/v1/routes/orders.routes.config.ts
@@ -1,6 +1,8 @@
 import { Application } from "express";
 import { CommonRoutesConfig } from "../../../common/common.routes.config";
 import ordersController from "../controllers/orders.controller";
+import validationMiddleware from "../middleware/validation.middleware";
+
 /**
  * Class representing routes related to orders.
  * Extends CommonRoutesConfig for common route configuration.
@@ -21,8 +23,22 @@ export class OrdersRoutes extends CommonRoutesConfig {
   configureRoutes(): Application {
     const routePrefix = "/api/v1/orders";
 
-    // health
+    // Index
     this.app.route(`${routePrefix}`).get(ordersController.index);
+
+    // Quotation
+    this.app
+      .route(`${routePrefix}`)
+      // validates all input fields in the request body
+      .all(validationMiddleware.validateOrder())
+      .post(ordersController.getQuotation);
+
+    // Create order
+    this.app
+      .route(`${routePrefix}/:quotationId`)
+      .all(validationMiddleware.validateOrder())
+      .post(ordersController.createOrder);
+
     return this.app;
   }
 }
