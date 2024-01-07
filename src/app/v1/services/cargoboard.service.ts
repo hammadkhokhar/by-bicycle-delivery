d432977ab3904224dbfaf878620ea3528483a0b0
hammadkhokhar <hello@hammadkhokhar.com>
2024-01-07 21:08:17 +0100

    add cargoboard distance service

---
+++ src/app/v1/services/cargoboard.service.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import logger from "../middleware/logger.middleware";
+import { IOrder } from "../interfaces/orders.interface";
+import axios, { AxiosRequestConfig } from "axios";
+
+class CargoboardServices {
+  async getDistance(orderRequest: IOrder): Promise<number> {
+    // Configure request
+    const distanceRequestConfig: AxiosRequestConfig = {
+      method: "get",
+      url: `https://distance.staging.cargoboard.com/distances/${orderRequest.shipper.address.shipperCountry}/${orderRequest.shipper.address.shipperPostcode}/${orderRequest.consignee.address.consigneeCountry}/${orderRequest.consignee.address.consigneePostcode}`,
+      headers: {
+        Authorization: `${process.env.CARGOBOARD_CREDENTIALS}`,
+      },
+    };
+
+    try {
+      // make request to distance service
+      const response = await axios.request(distanceRequestConfig);
+
+      // get distance from response
+      const distance: number = response.data.distance;
+
+      return distance;
+    } catch (error) {
+      // Log error
+      if (axios.isAxiosError(error)) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          logger.error(`Distance service: ${error.response.data}`);
+        } else if (error.request) {
+          // The request was made but no response was received
+          logger.error(`Distance service: ${error.request}, ${error.message}`);
+        }
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        const errorMessage = (error as Error).message;
+        logger.error(`Distance service: ${errorMessage}`);
+      }
+      return 0;
+    }
+  }
+}
+
+export default CargoboardServices;
