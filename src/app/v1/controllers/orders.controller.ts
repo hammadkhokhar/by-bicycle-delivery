480261c7ca095e4dcec7ee47fd782cbe1c763046
hammadkhokhar <hello@hammadkhokhar.com>
2024-01-07 21:09:34 +0100

    integrate distance service and order validations

--- src/app/v1/controllers/orders.controller.ts
+++ src/app/v1/controllers/orders.controller.ts
@@ -1,5 +1,12 @@
 import { Request, Response, response } from "express";
 import logger from "../middleware/logger.middleware";
+import { IOrder } from "../interfaces/orders.interface";
+import {
+  calculateDeliveryPrice,
+  validateRouteRange,
+} from "../helper/orders.helper";
+import CargoboardServices from "../services/cargoboard.service";
+
 /**
  * Controller class for handling orders-related requests.
  */
@@ -9,6 +16,47 @@ class OrdersController {
   }
 
   async getQuotation(req: Request, res: Response): Promise<void> {
+    const cb = new CargoboardServices();
+
+    // Log request
+    logger.info("Request", {
+      endpoint: req.originalUrl,
+      method: req.method,
+      body: req.body,
+    });
+    const orderRequest: IOrder = req.body;
+
+    let distance: number = 0,
+      price: number = 0;
+
+    try {
+      // Get Distance
+      let routeDistance = await cb.getDistance(orderRequest);
+
+      routeDistance = 50;
+
+      // Distance validation
+      let routeDistanceValidation = await validateRouteRange(routeDistance);
+
+      // If distance validation fails, send error message
+      if (!routeDistanceValidation.success) {
+        res.status(400).send({
+          message:
+            "The current operational range exceeds our limits, which are set between 3km and 300km. ",
+          distance: distance,
+          error: "Bad Request",
+        });
+        return;
+      }
+
+      // calculate price
+      price = await calculateDeliveryPrice(distance);
+
+      res.status(200).send("Quotation");
+    } catch (error) {
+      logger.error("Quotation Service Error", error);
+      res.status(500).send("Internal Server Error");
+    }
   }
 
   async createOrder(req: Request, res: Response): Promise<void> {
